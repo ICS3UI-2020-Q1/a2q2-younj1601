a8b3f2e1e836945b08e2b2e05961784adb94d883
younj1601 <younj1601@wrdsb.ca>
2020-09-15 15:13:55 +0000

    Finished question 2 of conditions

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program determines the bigger of 2 intergers
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask for one interger
+    System.out.println("Please enter an integer:");
+    //get the first interger
+    int num1 = input.nextInt();
     
+    //ask for next interger
+    System.out.println("Please enter another integer:");
+    //get the second interger
+    int num2 = input.nextInt();
+
+    if (num1 > num2){
+      //bigger number is num1
+      System.out.println("The biggest number is " + num1 + ".");
+    } else if (num1 < num2){
+      //bigger number is num2
+      System.out.println("The biggest number is " + num2 + ".");
+    } else{
+      //same number
+      System.out.println("There is no biggest number.");
+    }
   }
 }
